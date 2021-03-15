49547498363bad35a06c21f2a6c24adbd008aaae
Richard Byrne <richardbyrne1995@gmail.com>
2021-03-15 17:55:47 +0000

    Add Google Autocomplete to address search on homepage

---
+++ static/js/autocomplete.js
@@ -0,0 +1,22 @@
+let autocomplete;
+function initAutocomplete() {
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('address'),
+        {
+            componentRestrictions: {'country': ['IE']},
+            location: {lat: 53.350140, lng: -6.266155},
+            radius: 29053.48,
+            name: 'Dublin',
+        }
+    )
+    autocomplete.addListener('place_changed', OnPlaceChanged);
+}
+
+function OnPlaceChanged () {
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+        document.getElementById('address').placeholder = 'Enter a place';
+    } else {
+        document.getElementById('address').innerHTML = place.name;
+    }
+}
