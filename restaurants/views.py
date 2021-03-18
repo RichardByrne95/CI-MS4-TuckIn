6aaecfe3ac45266b3c91a4800301aaed6580ec39
Richard Byrne <richardbyrne1995@gmail.com>
2021-03-18 18:05:09 +0000

    Set up basic restaurants.html and set url

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Restaurant
 
 # Create your views here.
+
+def all_restaurants(request):
+    restaurants = Restaurant.objects.all()
+    context =  {
+        'restaurants': restaurants,
+    }
+    return render(request, 'restaurants/restaurants.html', context)
