28ea66b70952790773e6ef601f9272550c4ec1c3
Richard Byrne <richardbyrne1995@gmail.com>
2021-03-18 16:33:40 +0000

    Customise admin

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
 from .models import Restaurant, Cuisine, MenuSection, FoodItem
 
-# Register your models here.
-admin.site.register(Restaurant)
+class RestaurantsAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'cuisine',
+        'rating',
+        'delivery_charge',
+    )
+
+    ordering = ('name',)
+
+class MenuSectionAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+        'restaurant',
+    )
+
+    ordering = ('name',)
+
+class FoodItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+        'menu_section',
+        'description',
+        'price',
+        'is_vegetarian',
+        'is_vegan',
+    )
+
+    ordering = ('name',)
+
+admin.site.register(Restaurant, RestaurantsAdmin)
 admin.site.register(Cuisine)
-admin.site.register(MenuSection)
-admin.site.register(FoodItem)
+admin.site.register(MenuSection, MenuSectionAdmin)
+admin.site.register(FoodItem, FoodItemAdmin)
