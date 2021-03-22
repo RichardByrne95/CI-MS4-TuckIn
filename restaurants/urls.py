74e82e8c21267d90a63c53727e350c1861221469
Richard Byrne <richardbyrne1995@gmail.com>
2021-03-22 11:16:31 +0000

    Create restaurant menu header

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_restaurants, name='restaurants')
+    path('', views.all_restaurants, name='restaurants'),
+    path('<int:restaurant_id>/', views.restaurant_menu, name='restaurant_menu'),
 ]
