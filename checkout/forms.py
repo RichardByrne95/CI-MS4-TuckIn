822b8375453a950dab0b0c19ab000a8a0de7e563
Richard Byrne <richardbyrne1995@gmail.com>
2021-03-26 15:53:49 +0000

    Add order form, admin and signals

---
+++ checkout/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Order
+
+# Referenced Boutique Ado
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number', 'postcode',
+                  'address_1', 'address_2',)
+    
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'postcode': 'Postal Code',
+            'address_1': 'Address 1',
+            'address_2': 'Address 2',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
