60853c4fede9633d5c840e96c21cd4a0df04ee3f
Richard Byrne <richardbyrne1995@gmail.com>
2021-04-02 19:25:34 +0100

    Create basic food item modal with quantity selector

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
+    path('order_confirmation/', views.order_confirmation, name='order_confirmation'),
 ]
---
+++ static/js/foodModal.js
@@ -0,0 +1,35 @@
+// Change modal content based on food item
+$(".food-item-card-body").on("click", function () {
+    let foodName = $(this).find(".food-item-card-name").html()
+    let foodImageSrc = $(this).find(".food-item-photo").attr("src")
+    let foodImageAlt = $(this).find(".food-item-photo").attr("alt")
+
+    $("#modal-label").text(foodName)
+    $("#food-item-modal-image").attr("src", foodImageSrc)
+    $("#food-item-modal-image").attr("alt", foodImageAlt)
+});
+
+// Decrement Quantity
+$(".decrement-qty").on("click", function () {
+    let currentVal = $(".qty-input").val()
+    let newVal = Number(currentVal) - 1
+
+    // Prevent user from going below 0
+    if (newVal < 0) {
+        $(".qty-input").val(0)
+    } else {
+        $(".qty-input").val(newVal)
+    }
+});
+
+$(".increment-qty").on("click", function () {
+    let currentVal = $(".qty-input").val()
+    let newVal = Number(currentVal) + 1
+    
+    // Prevent user from going above 15
+    if (newVal > 15) {
+        $(".qty-input").val(15)
+    } else {
+        $(".qty-input").val(newVal)
+    }
+});
