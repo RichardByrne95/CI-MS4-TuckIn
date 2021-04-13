c845daed98b37d09e78536c85c8692defbebe5da
Richard Byrne <richardbyrne1995@gmail.com>
2021-04-13 15:22:40 +0100

    Fix food modal image bug

--- static/js/foodModal.js
+++ static/js/foodModal.js
@@ -3,13 +3,14 @@ $(".food-item-card-body").on("click", function (e) {
     let foodName = $(this).find(".food-item-card-name").html()
     let foodImageSrc = $(this).find(".food-item-photo").attr("src")
     let foodImageAlt = $(this).find(".food-item-photo").attr("alt")
-
-    let foodID = $(this).find(".food-id").html()
-    $("#this-food").attr("value", foodID)
-
+    
     $("#modal-label").text(foodName)
     $("#food-item-modal-image").attr("src", foodImageSrc)
     $("#food-item-modal-image").attr("alt", foodImageAlt)
+
+    // Passing this food's id to python via hidden input field
+    let foodID = $(this).find(".food-id").html()
+    $("#this-food").attr("value", foodID)
 });
 
 // Decrement Quantity
