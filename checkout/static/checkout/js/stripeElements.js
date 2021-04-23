2685e729ff2d0941282f909cd42cb362117c9629
Richard Byrne <richardbyrne1995@gmail.com>
2021-04-23 09:47:49 +0100

    Add Stripe confirm card payment functionality

--- checkout/static/checkout/js/stripeElements.js
+++ checkout/static/checkout/js/stripeElements.js
@@ -5,7 +5,7 @@ let stripe = Stripe(stripePublicKey);
 let elements = stripe.elements();
 let cardElement = $("#card-element");
 
-const style =  {
+const style = {
     base: {
         color: "#000",
         fontFamily: "'Inter', sans-serif",
@@ -21,15 +21,15 @@ const style =  {
     }
 };
 
-let card = elements.create("card", {style: style});
+let card = elements.create("card", { style: style });
 card.mount("#card-element");
 
-card.on('change', function(event) {
+card.on('change', function (event) {
     var displayError = document.getElementById('card-errors');
     if (event.error) {
-      displayError.textContent = event.error.message;
+        displayError.textContent = event.error.message;
     } else {
-      displayError.textContent = '';
+        displayError.textContent = '';
     }
 });
 
@@ -47,3 +47,39 @@ card.addEventListener('change', (event) => {
         errorDiv.textContent = "";
     }
 })
+
+// Form submission
+let form = $("#payment-form");
+
+form.addEventListener('submit', (e) => {
+    // Prevent posting so that the below code can be executed
+    e.preventDefault();
+    // Disable card and submit button to prevent multiple submissions
+    card.update({ "disabled": true });
+    $("#submit-button").attr("disabled", true);
+    // Send card details to Stripe
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then((result) => {
+        if (result.error) {
+            // If error, show error to customer
+            let errorDiv = $("#card-errors");
+            let html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            // Re-enable card and submit button to allow the user to try again
+            card.update({ "disabled": false });
+            $("#submit-button").attr("disabled", false);
+        } else {
+            // If payment has gone through, submit form
+            if (result.paymentIntent.status === "succeeded") {
+                form.submit();
+            }
+        }
+    })
+});
--- checkout/views.py
+++ checkout/views.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from restaurants.models import Restaurant
 from checkout.forms import OrderForm
 from profiles.models import CustomerProfile
+from django.contrib import messages
+import stripe
 
 
 def checkout_address(request):
@@ -100,11 +102,21 @@ def checkout_payment(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
 
+    # Stripe
+    stripe_total = round(total * 100) # Stripe requires an integer when charging
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    if not settings.STRIPE_PUBLIC_KEY:
+        messages.warning(request, 'Stripe public key is missing.')
+
     context = {
         'order_form': order_form,
         'delivery_time': delivery_time,
         'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
-        'client_secret': 'test',
+        'client_secret': intent.client_secret,
     }
     return render(request, 'checkout/checkout_payment.html', context)
 
