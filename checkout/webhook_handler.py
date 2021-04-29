05b844f521932b259fc8544ee13128bc81960534
Richard Byrne <richardbyrne1995@gmail.com>
2021-04-29 12:06:17 +0100

    Create payment intent webhook handlers

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -6,8 +6,22 @@ class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
 
-    # Stripe Webhook event handler
+    # Unhandled webhook event handler
     def handle_event(self, event):
+        return HttpResponse(
+            content=f'Webhook received: {event[type]}',
+            status=200,
+        )
+
+    # Payment Intent Succeeded webhook event handler
+    def handle_payment_intent_succeeded(self, event):
+        return HttpResponse(
+            content=f'Webhook received: {event[type]}',
+            status=200,
+        )
+
+    # Payment Intent Payment Failed webhook event handler
+    def handle_payment_intent_payment_failed(self, event):
         return HttpResponse(
             content=f'Webhook received: {event[type]}',
             status=200,
