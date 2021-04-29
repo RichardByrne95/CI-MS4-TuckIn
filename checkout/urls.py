25ad1520669c6f0f9474bab9b9b933997cfbe669
Richard Byrne <richardbyrne1995@gmail.com>
2021-04-29 12:53:28 +0100

    Create webhooks url and basic webhook handler function

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('address/', views.checkout_address, name='checkout_address'),
     path('time/', views.checkout_time, name='checkout_time'),
     path('payment/', views.checkout_payment, name='checkout_payment'),
     path('order-confirmation/<order_number>/', views.order_confirmation, name='order_confirmation'),
+    path('webhooks/', webhook, name='webhook'),
 ]
