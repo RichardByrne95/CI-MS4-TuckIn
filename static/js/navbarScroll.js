a0b8f2b9d428884fb14d7365034a8f8a10f6651d
Richard Byrne <richardbyrne1995@gmail.com>
2021-05-13 16:52:37 +0100

    Fix navbar gradient not coming back after scrolling back to top of page

--- static/js/navbarScroll.js
+++ static/js/navbarScroll.js
@@ -2,18 +2,18 @@
 // Change navbar based on scroll position
 window.onscroll = () => {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        $("header").css({"background-image": "none"});
-        $(".navbar").css({"background-color": "#fafafa"});
-        $(".navbar-brand").css({"color": "orangered"});
-        $(".search-icon").css({"color": "#000"});
-        $(".nav-link").css({"color": "#00baff"});
-        $(".fa-shopping-bag").css({"color": "#00baff"});
+        $("header").addClass("block-colour").removeClass("transparent-gradient");
+        $(".navbar").addClass("navbar-block-colour");
+        $(".navbar-brand").addClass("navbar-brand-block-colour");
+        $(".search-icon").addClass("search-icon-block-colour");
+        $(".nav-link").addClass("nav-link-colour");
+        $(".fa-shopping-bag").addClass("fa-shopping-bag-block-colour");
     } else {
-        $("header").css({"background-image": "linear-gradient(to bottom, rgba(0,0,0,.7)"});
-        $(".navbar").css({"background-color": "rgba(0,0,0,0)"});
-        $(".navbar-brand").css({"color": "#fafafa"});
-        $(".search-icon").css({"color": "#fafafa"});
-        $(".nav-link").css({"color": "#fafafa"});
-        $(".fa-shopping-bag").css({"color": "#fafafa"});
+        $("header").removeClass("block-colour").addClass("transparent-gradient");
+        $(".navbar").removeClass("navbar-block-colour");
+        $(".navbar-brand").removeClass("navbar-brand-block-colour");
+        $(".search-icon").removeClass("search-icon-block-colour");
+        $(".nav-link").removeClass("nav-link-colour");
+        $(".fa-shopping-bag").removeClass("fa-shopping-bag-block-colour");
     }
 }
