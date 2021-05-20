d7e13aedb284f1f74a14cd00cd919774d3b8c11d
Richard Byrne <richardbyrne1995@gmail.com>
2021-05-20 09:08:18 +0100

    Create cookie and privacy policy urls and views

--- footer_links/urls.py
+++ footer_links/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('list_your_restaurant/', views.list_your_restaurant,
          name='list_your_restaurant'),
+    path('cookies_policy/', views.cookies_policy,
+         name='cookies_policy'),
+    path('privacy_policy/', views.privacy_policy,
+         name='privacy_policy'),
 ]
--- footer_links/views.py
+++ footer_links/views.py
@@ -9,3 +9,13 @@ def list_your_restaurant(request):
         'dynamic_navbar': True,
     }
     return render(request, 'footer_links/list_your_restaurant.html', context)
+
+
+def cookies_policy(request):
+    context = {}
+    return render(request, 'footer_links/cookies_policy.html', context)
+
+
+def privacy_policy(request):
+    context = {}
+    return render(request, 'footer_links/privacy_policy.html', context)
