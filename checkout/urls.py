e0528d90eb17d94a1ced8daccebb1b37b87da0a4
Richard Byrne <richardbyrne1995@gmail.com>
2021-06-22 17:30:04 +0100

    Update webhook URL

--- checkout/urls.py
+++ checkout/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
          views.order_confirmation, name='order_confirmation'),
     path('cache_checkout_data/', views.cache_checkout_data,
          name='cache_checkout_data'),
-    path('wh/', webhook, name='webhook'),
+    path('webhook/', webhook, name='webhook'),
 ]
