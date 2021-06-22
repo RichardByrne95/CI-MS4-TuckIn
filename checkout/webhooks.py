3e6629f74ccf4ab0788a3ce9739d907358142dab
Richard Byrne <richardbyrne1995@gmail.com>
2021-06-22 18:54:14 +0100

    Use Stripe Webhook contruct_event and return HttpResponse

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -17,6 +17,8 @@ def webhook(request):
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
+    print(wh_secret, stripe.api_key)
+
     # Get webhook data and verify its signature
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
@@ -24,12 +26,10 @@ def webhook(request):
 
     try:
         # Get webhook from Stripe
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, wh_secret
-        )
+        event = stripe.Webhook.construct_event(payload, sig_header, wh_secret)
     except ValueError as e:
         # Invalid payload exception handler
-        return HttpResponse(status=400)
+        return HttpResponse(content=e, status=400)
     except Exception as e:
         # Generic exception handler
         return HttpResponse(content=e, status=400)
@@ -50,4 +50,4 @@ def webhook(request):
 
     # Call the event handler with the event
     response = event_handler(event)
-    return response
+    return HttpResponse(content=response, status=200)
