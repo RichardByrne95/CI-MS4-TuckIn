f6257da235535882061b8059c710c4f8abfc7d64
Richard Byrne <richardbyrne1995@gmail.com>
2021-06-22 17:10:42 +0100

    Use Stripe Webhook construct_event

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -11,18 +11,21 @@ from django.views.decorators.http import require_POST
 @csrf_exempt  # Stripe doesn't send CSRF tokens
 @require_POST  # Only react to post requests, reject GET requests
 def webhook(request):
+    print('Webhook received')
+
     # Set variables
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
     # Get webhook data and verify its signature
     payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
 
     try:
         # Get webhook from Stripe
-        event = stripe.Event.construct_from(
-            json.loads(payload), stripe.api_key
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, wh_secret
         )
     except ValueError as e:
         # Invalid payload exception handler
