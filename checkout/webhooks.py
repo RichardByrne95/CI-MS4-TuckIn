ab818022df616e4b251512617b527b8762788918
Richard Byrne <richardbyrne1995@gmail.com>
2021-06-22 16:17:23 +0100

    Use sig_header and construct_event for Stripe webhook

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,4 +1,3 @@
-import json
 import stripe
 from django.conf import settings
 from django.http import HttpResponse
@@ -8,21 +7,22 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
 # Referenced https://stripe.com/docs/webhooks/build#example-code
-
-
 @csrf_exempt  # Stripe doesn't send CSRF tokens
 @require_POST  # Only react to post requests, reject GET requests
 def webhook(request):
     # Set variables
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    # Get webhook data and verify its signature
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     payload = request.body
     event = None
 
     try:
         # Get webhook from Stripe
-        event = stripe.Event.construct_from(
-            json.loads(payload), stripe.api_key
+        event = stripe.Event.construct_event(
+            payload, sig_header, wh_secret
         )
     except ValueError as e:
         # Invalid payload exception handler
@@ -30,19 +30,6 @@ def webhook(request):
     except Exception as e:
         # Generic exception handler
         return HttpResponse(content=e, status=400)
-
-    # # Handle the event
-    # if event.type == 'payment_intent.succeeded':
-    #     payment_intent = event.data.object  # contains a stripe.PaymentIntent
-    #     # Then define and call a method to handle the successful payment intent.
-    #     # handle_payment_intent_succeeded(payment_intent)
-    # elif event.type == 'payment_method.attached':
-    #     payment_method = event.data.object  # contains a stripe.PaymentMethod
-    #     # Then define and call a method to handle the successful attachment of a PaymentMethod.
-    #     # handle_payment_method_attached(payment_method)
-    # # ... handle other event types
-    # else:
-    #     print('Unhandled event type {}'.format(event.type))
 
     # Setup webhook handler
     handler = StripeWH_Handler(request)
