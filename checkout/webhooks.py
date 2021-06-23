ad17f54d259482f6a855b94cb259700a38ba7f31
Richard Byrne <richardbyrne1995@gmail.com>
2021-06-23 22:28:28 +0100

    Remove handler, just return http response

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,4 +1,3 @@
-import json
 import stripe
 from django.conf import settings
 from django.http import HttpResponse
@@ -8,8 +7,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
 # Referenced https://stripe.com/docs/webhooks/build#example-code
+@require_POST  # Only react to post requests, reject GET requests
 @csrf_exempt  # Stripe doesn't send CSRF tokens
-# @require_POST  # Only react to post requests, reject GET requests
 def webhook(request):
     # Set variables
     wh_secret = settings.STRIPE_WH_SECRET
@@ -18,13 +17,16 @@ def webhook(request):
     signature = request.headers.get('stripe-signature')
 
     # Get webhook data and verify its signature
-    payload = request.data
+    payload = request.body
     event = None
 
     try:
         # Get webhook from Stripe
         event = stripe.Webhook.construct_event(
-            payload=request.data, sig_header=signature, secret=wh_secret
+            payload=payload,
+            sig_header=signature,
+            secret=wh_secret,
+            api_key=stripe.api_key
         )
     # Invalid payload exception handler
     except ValueError as e:
@@ -36,20 +38,4 @@ def webhook(request):
     except Exception as e:
         return HttpResponse(content=e, status=400)
 
-    # Setup webhook handler
-    handler = StripeWH_Handler(request)
-
-    # Map webhook events to relevant handler functions
-    event_map = {
-        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
-    }
-
-    # Get webhook type from Stripe
-    event_type = event['type']
-    # Use generic handler if not available
-    event_handler = event_map.get(event_type, handler.handle_event)
-
-    # Call the event handler with the event
-    response = event_handler(event)
-    return HttpResponse(content=response, status=200)
+    return HttpResponse(content='Success', status=200)
