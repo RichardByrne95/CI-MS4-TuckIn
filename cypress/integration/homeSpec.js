49589840e952f9095e4769d28c228efe8d6bdb19
Richard Byrne <richardbyrne1995@gmail.com>
2021-07-05 14:20:36 +0100

    Create tests to check for valid address input and cuisine with no address

--- cypress/integration/homeSpec.js
+++ cypress/integration/homeSpec.js
@@ -1,4 +1,4 @@
-describe('Home Page/Index Test', () => {
+describe('Home Page/Index Tests', () => {
     // Page Load Test
     it('successfully loads', () => {
         cy.visit('/');
@@ -6,31 +6,45 @@ describe('Home Page/Index Test', () => {
     });
 
     // Address Tests
-    it('should disable find button when new user visits site', () => {
+    it('disables find button when new user visits site', () => {
         cy.get('button.find-button').should('be.disabled');
     });
 
-    it('should enable find button upon a valid address being entered or selected', () => {
-        cy.get('input#address').type('22 Shelbourne Road', { force: true });
-        cy.get('input#address').focus();
+    it('enables find button upon a valid address being entered or selected', () => {
+        cy.get('input#address').type('22 Shelbourne Road', { force: true }).focus();
         cy.get('.pac-item').first().click();
         cy.get('button.find-button').should('not.be.disabled');
     });
 
-    it('should disable find button if input is empty', () => {
+    it('disables find button if input is empty', () => {
         cy.get('input#address').clear({ force: true });
         cy.get('button.find-button').should('be.disabled', { force: true });
     });
 
-    it('should return to homepage and display error if "Dublin" is not in address', () => {
+    it('returns to homepage and display error if "Dublin" is not in address', () => {
         cy.get('input#address').clear({ force: true }).type('Cork', { force: true }).get('input#address').focus();
-        cy.get('.pac-item').first().click();
-        cy.wait(1000);
-        cy.get('#address-finder').submit();
-        cy.wait(1000);
+        cy.get('.pac-item').first().click().wait(1000);
+        cy.get('#address-finder').submit().wait(1000);
         cy.url().should('equal', 'http://127.0.0.1:8000/');
         cy.get('.toast').should('have.class', 'show');
         cy.get('.toast-header').children('strong').should('contain', 'Error');
         cy.get('.toast-body').children().should('contain', 'Address not found within Dublin, please try again');
+    });
+
+    it('redirects to all restaurants page and display "Dublin" as address if no address inputted and cuisine selected', () => {
+        cy.get('i.fa-pizza-slice').click();
+        cy.url().should('include', 'restaurants').should('contain', 'cuisine=pizza');
+        cy.get('h4.nearby-restaurants').should('contain', 'delivering in Dublin');
+        cy.get('.change-location > a').should('contain', 'set location');
+    });
+
+    it('redirects to all restaurants page and display valid address on page if valid address', () => {
+        cy.visit('/');
+        cy.get('input#address').clear({ force: true }).type('22 Shelbourne Road', { force: true }).focus();
+        cy.get('.pac-item').first().click().wait(1000);
+        cy.get('#address-finder').submit().wait(1000);
+        cy.url().should('equal', 'http://127.0.0.1:8000/restaurants/');
+        cy.get('h4.nearby-restaurants').should('contain', 'delivering to 22 Shelbourne Road');
+        cy.get('.change-location > a').should('contain', 'change location');
     });
 });
