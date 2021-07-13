24d9ff39841e7a36dba0b4c21c432c9bc20bb3a2
Richard Byrne <richardbyrne1995@gmail.com>
2021-07-13 13:18:22 +0100

    Make homepage exempt from xframe limits

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from restaurants.models import Cuisine
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 
+@xframe_options_exempt
 def home(request):
     cuisines = Cuisine.objects.all()
     context = {
