e4a504502d0421b7131525b84d60950e5fc084cb
Richard Byrne <richardbyrne1995@gmail.com>
2021-08-17 14:30:33 +0100

    Fix order confirmation url without underscore

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('address/', views.checkout_address, name='checkout_address'),
     path('time/', views.checkout_time, name='checkout_time'),
     path('payment/', views.checkout_payment, name='checkout_payment'),
-    path('order-confirmation/<order_number>/',
+    path('order_confirmation/<order_number>/',
          views.order_confirmation, name='order_confirmation'),
     path('cache_checkout_data/', views.cache_checkout_data,
          name='cache_checkout_data'),
