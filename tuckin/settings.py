5c929b6b0f9afd26b0adc492eff0e14fa4a0959b
Richard Byrne <richardbyrne1995@gmail.com>
2021-08-17 16:36:34 +0100

    Reintroduce conditional DEBUG based on DEVELOPMENT env var

--- tuckin/settings.py
+++ tuckin/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['https://tuckin-ms4.herokuapp.com/', 'tuckin-ms4.herokuapp.com', 'localhost', '127.0.0.1']
 
